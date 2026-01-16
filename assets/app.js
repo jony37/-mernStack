c62cdeda462068640078fa7b656b7f1a6866204f
jony <ravshanovjony7@gmail.com>
2026-01-17 00:05:33 +0500

    Malumot turlari

--- assets/app.js
+++ assets/app.js
@@ -3,8 +3,48 @@
 // let aga = 22;
 // const age = 22;
 
-
 // console.log(a); // undefined
 // var a = 22
 // var a = 22
-// var a = 22
+
+// {
+// // var a = 22
+// }
+
+// 4 dars Malumot turlari
+
+// Primitive
+// const age = 23;
+// console.log(typeof age); // Number
+
+// const name = "Jonpo'lat";
+// console.log(typeof name); // String
+
+// const bolean = true;
+// console.log(typeof bolean); // boolean
+
+// let a;
+// console.log(a); // undefind
+
+// let nol = null;
+// console.log(nol); // null
+
+// let id = Symbol(121212121);
+// console.log(id); // Symbol
+
+// let qol = BigInt(190489489012477124792184793120472104899991209);
+// console.log(qol); // BigInt
+
+// // Object
+
+// const person = {
+//   name: "jonpolat",
+//   age: "19",
+//   hobby: {
+//     sport: "Play chess",
+//   },
+// };
+
+// // Array
+
+// const colors = ["black", 22, true, {}]
